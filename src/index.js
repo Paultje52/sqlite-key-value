71a322cd65f1211a97e114b8864d0cf9640d068c
Paul <paultje52@gmail.com>
2020-07-24 12:30:57 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = class DataBase {
-  constructor({development = false, cwd = __dirname, name ="database"} = {development: false, cwd: __dirname, name: "database"}) {
+  constructor({development = false, cwd = __dirname, name ="database", inMemory: false} = {development: false, cwd: __dirname, name: "database", inMemory: false}) {
     if (development) {
       this.database = new (require("json-config-store"))({
         cwd: cwd,
@@ -15,8 +15,9 @@ module.exports = class DataBase {
     let i = setInterval(() => {
       if (!this.sqlite.open) return;
       clearInterval(i);
-      this.sqlite.run("CREATE TABLE IF NOT EXISTS cn_bot_table (key TEXT, value TEXT);", () => {
-        this.sqlite.all("SELECT * FROM cn_bot_table;", (err, data) => {
+      this.sqlite.run("CREATE TABLE IF NOT EXISTS keyvalue (key TEXT, value TEXT);", () => {
+        if (!inMemory) return this.ready = true;
+        this.sqlite.all("SELECT * FROM keyvalue;", (err, data) => {
           if (err) throw err;
           data.forEach(d => {
             this.memory[d.key] = JSON.parse(d.value);
@@ -40,7 +41,7 @@ module.exports = class DataBase {
   get(key) {
     if (this.memory[key]) return this.memory[key];
     return new Promise((res) => {
-      this.sqlite.all(`SELECT value FROM cn_bot_table WHERE key='${key}';`, [], (err, data) => {
+      this.sqlite.all(`SELECT value FROM keyvalue WHERE key='${key}';`, [], (err, data) => {
         if (err) throw err;
         if (data.length === 0) return res(undefined);
         res(data[0].key);
@@ -51,11 +52,11 @@ module.exports = class DataBase {
   set(key, value) {
     this.memory[key] = value;
     return new Promise((res) => {
-      this.sqlite.all(`SELECT * FROM cn_bot_table where key='${key}'`, [], (err, data) => {
+      this.sqlite.all(`SELECT * FROM keyvalue where key='${key}'`, [], (err, data) => {
         if (err) throw err;
         let sql = "";
-        if (data.length === 0) sql = `INSERT INTO cn_bot_table (key, value) VALUES ('${key}', '${JSON.stringify(value)}')`;
-        else sql = `UPDATE cn_bot_table SET value='${JSON.stringify(value)}' WHERE key='${key}'`;
+        if (data.length === 0) sql = `INSERT INTO keyvalue (key, value) VALUES ('${key}', '${JSON.stringify(value)}')`;
+        else sql = `UPDATE keyvalue SET value='${JSON.stringify(value)}' WHERE key='${key}'`;
         this.sqlite.all(sql, [], (err) => {
           if (err) throw err;
           res(true);
@@ -67,7 +68,7 @@ module.exports = class DataBase {
   delete(key) {
     delete this.memory[key];
     return new Promise((res) => {
-      this.sqlite.all(`DELETE FROM cn_bot_table WHERE key='${key}'`, [], (err) => {
+      this.sqlite.all(`DELETE FROM keyvalue WHERE key='${key}'`, [], (err) => {
         if (err) throw err;
         res(true);
       });
